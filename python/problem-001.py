701bf6d10c289ccc3a7f9504b55f2d465c84a509
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2021-03-13 15:50:31 +0000

    Add timing of function (not so helpful for such a tiny function though)

--- python/problem-001.py
+++ python/problem-001.py
@@ -5,11 +5,17 @@ The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 import numpy as np
+import time
 
 if __name__ == "__main__":
+    start_time = time.time()
+
     qualifying_multiples = []
     for i in range(1000):
         if i % 3 == 0 or i % 5 == 0:
             qualifying_multiples.append(i)
     answer = np.sum(qualifying_multiples)
+    elapsed_time = time.time() - start_time
+    
+    print("Time elapsed:", elapsed_time)
     print("Answer to problem 001: ", answer)
