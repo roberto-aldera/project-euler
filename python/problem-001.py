fca69527a93d09d2470928542fb97f710b1d8964
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2021-03-13 16:06:18 +0000

    Simplyfy solution (we don't need a list or numpy here)

--- python/problem-001.py
+++ python/problem-001.py
@@ -4,18 +4,17 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
-import numpy as np
 import time
 
 if __name__ == "__main__":
     start_time = time.time()
 
-    qualifying_multiples = []
+    sum_of_qualifying_multiples = 0
     for i in range(1000):
         if i % 3 == 0 or i % 5 == 0:
-            qualifying_multiples.append(i)
-    answer = np.sum(qualifying_multiples)
+            sum_of_qualifying_multiples += i
+    answer = sum_of_qualifying_multiples
     elapsed_time = time.time() - start_time
-    
+
     print("Time elapsed:", elapsed_time)
     print("Answer to problem 001: ", answer)
