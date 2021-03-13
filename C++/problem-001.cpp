c21a5dd8cd923a442b11bbc57a42fe44e7edf736
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2021-03-13 16:53:22 +0000

    Fix indenting and formatting for cpp file

--- C++/problem-001.cpp
+++ C++/problem-001.cpp
@@ -1,15 +1,16 @@
-// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-// Find the sum of all the multiples of 3 or 5 below 1000.
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+// get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the
+// multiples of 3 or 5 below 1000.
 
 #include <iostream>
 
 int main() {
-	int sum_of_multiples = 0;
+  int sum_of_multiples = 0;
 
-	for (auto i = 0; i < 1000; ++i) {
-		if(i % 3 == 0 || i % 5 == 0) {
-			sum_of_multiples += i;
-		}
-	}
-	std::cout << "Answer to problem 001: " << sum_of_multiples << std::endl;
+  for (auto i = 0; i < 1000; ++i) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum_of_multiples += i;
+    }
+  }
+  std::cout << "Answer to problem 001: " << sum_of_multiples << std::endl;
 }
