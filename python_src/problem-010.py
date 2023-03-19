5d65756dbadacccd58c9f83a9eb1a0bb4f1f486d
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2023-03-19 15:33:44 +0200

    Add problem-010

---
+++ python_src/problem-010.py
@@ -0,0 +1,29 @@
+"""
+Problem 010
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
+"""
+
+
+def sieveOfEratosthenes(upper_limit):
+    list_of_primes = []
+    prime = [True for i in range(upper_limit+1)]
+    p = 2
+    while (p * p <= upper_limit):
+
+        # If prime[p] has not yet been ruled out, then it's prime
+        if (prime[p] == True):
+            # Updating all multiples of p
+            for i in range(p * p, upper_limit+1, p):
+                prime[i] = False
+        p += 1
+
+    for p in range(2, upper_limit+1):
+        if prime[p]:
+            list_of_primes.append(p)
+    return list_of_primes
+
+
+if __name__ == "__main__":
+
+    print("Answer:", sum(sieveOfEratosthenes(200)))
